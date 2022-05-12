731afc2c0bc4b1d91c58022fdf536b16da271688
Lucas Israel <lisraelalves92@gmail.com>
2022-05-12 18:01:50 -0300

    exemplos de operadores NOT

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsOperadoreslogicos.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsOperadoreslogicos.js
@@ -59,3 +59,35 @@
 //     message = "FINALMENTE, descanso merecido UwU"; // GZUS, UwU?????
 // }
 // console.log(message);
+
+//--------------
+
+// Operador NOT
+
+// Vamos iniciar com um exemplo do que o NOT é capaz, você consegue prever o que esse código vai imprimir?
+// Copiar
+//   console.log((2 + 2) === 4);
+// Isso aqui deve nos retornar true, não é? Afinal, 2 + 2 resultar em 4 é uma declaração verdadeira. Agora, se adicionarmos o operador NOT antes dessa declaração?
+// Copiar
+//   console.log(!(2 + 2) === 4);
+// Se você executar esse código, vai perceber que o valor impresso é o oposto do anterior. 🤔
+// Estamos diante de um operador muito poderoso. Ele pode inverter o valor booleano de um elemento. Isso mesmo!! Se tivermos uma variável ou valor considerado true, podemos gerar o resultado oposto simplesmente fazendo !variável. Ou seja, false.
+// Então, sabendo que o resultado original da operação ali em cima é true, quando a gente insere o operador NOT antes da operação, teremos o valor contrário a nossa resposta final, que é false.
+// Vale lembrar que o conceito de truthy e falsy também se aplica aqui, por isso não estamos limitados apenas aos tipos primitivos booleanos. Podemos usar em:
+// Strings...
+// Copiar
+// const squirtle = "melhor pokemon inicial";
+
+// console.log(!squirtle); // false
+// Números...
+// Copiar
+// console.log(!42); // false
+
+// console.log(!0); // true
+// // O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+// Valores nulos...
+// Copiar
+// console.log(!null); // true
+// Valores indefinidos..
+// Copiar
+// console.log(!undefined); // true
