fd1d5dca5d9235721d33518bbf700c6ced25f06b
Lucas Israel <lisraelalves92@gmail.com>
2022-05-12 15:20:44 -0300

    removido pushinicial.txt e feito conteudo variavel

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsVariaveis.js
@@ -0,0 +1,11 @@
+const myName = "Lucas";
+const birthCity = "São Caetano do Sul";
+let birthYear = "1992";
+birthYear = "2030";
+birthCity = "Santo andre"
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+// Erro no birthCity por ela nao poder mudar (uma constante)
