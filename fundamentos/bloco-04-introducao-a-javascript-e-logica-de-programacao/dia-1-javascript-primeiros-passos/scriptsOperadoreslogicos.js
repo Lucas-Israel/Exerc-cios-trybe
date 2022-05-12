624d314705130ffae1af3f27f5b06c67f86806e4
Lucas Israel <lisraelalves92@gmail.com>
2022-05-12 16:10:12 -0300

    feito operador AND

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsOperadoreslogicos.js
@@ -0,0 +1,29 @@
+// const comida = 'pão na chapa';
+// const bebida = 'cafezinho';
+
+// if (bebida === 'cafezinho' && comida === 'pão na chapa') {
+//   console.log('Muito obrigado pela refeição :)');
+// } else {
+//   console.log('Acho que houve um engano com meu pedido');
+// }
+
+const currentHour = 11;
+let message = "";
+
+if (currentHour >= 22){
+    message = "Não deveríamos comer nada, é hora de dormir";
+}
+else if (currentHour >= 18 && currentHour < 22){
+    message = "Rango da noite, vamos jantar :D";
+}
+else if (currentHour >= 14 && currentHour < 18){
+    message = "Vamos fazer um bolo pro café da tarde?";
+}
+else if(currentHour >= 11 && currentHour < 14){
+    message = "Hora do almoço!!!";
+}
+else if (currentHour >= 4 && currentHour < 11){
+    message = "Hmmm, cheiro de café recém passado";
+}
+
+console.log(message);
