4c6648866f8ba400635c6ab55807b265b1d1b4be
Lucas Israel <lisraelalves92@gmail.com>
2022-05-12 15:52:18 -0300

    condicoes if else

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsIfElse.js
@@ -0,0 +1,11 @@
+const nota = 59;
+
+if (nota >= 80){
+  console.log ("Nota: "+nota+"! Parabéns, você foi aprovada(o)!" );
+}
+else if (nota < 80 && nota >= 60){
+  console.log ("Nota: "+nota+", você está na nossa lista de espera" );
+}
+else if (nota < 60){
+  console.log ("Nota: "+nota+", você foi reprovada(o)" )
+}
