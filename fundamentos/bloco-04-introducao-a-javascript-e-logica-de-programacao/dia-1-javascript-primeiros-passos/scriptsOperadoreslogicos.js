995518634b951c048bf21d5bab988491df103e68
Lucas Israel <lisraelalves92@gmail.com>
2022-05-12 16:24:42 -0300

    operadores logicos or, horario de aula

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsOperadoreslogicos.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scriptsOperadoreslogicos.js
@@ -7,23 +7,49 @@
 //   console.log('Acho que houve um engano com meu pedido');
 // }
 
-const currentHour = 11;
+
+//  AND
+// const currentHour = 11;
+// let message = "";
+
+// if (currentHour >= 22){
+//     message = "Não deveríamos comer nada, é hora de dormir";
+// }
+// else if (currentHour >= 18 && currentHour < 22){
+//     message = "Rango da noite, vamos jantar :D";
+// }
+// else if (currentHour >= 14 && currentHour < 18){
+//     message = "Vamos fazer um bolo pro café da tarde?";
+// }
+// else if(currentHour >= 11 && currentHour < 14){
+//     message = "Hora do almoço!!!";
+// }
+// else if (currentHour >= 4 && currentHour < 11){
+//     message = "Hmmm, cheiro de café recém passado";
+// }
+
+// console.log(message);
+//--------
+
+// OR
+
+// const bebidaPrincipal =  'cafezinho';
+// const bebidaAlternativa = 'suco de laranja';
+
+// if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+//   console.log("Obrigado por me atender :D")
+// } else {
+//   console.log("Ei, eu não pedi isso!");
+// }
+
+let weekDay = "quarta-feira";
 let message = "";
 
-if (currentHour >= 22){
-    message = "Não deveríamos comer nada, é hora de dormir";
-}
-else if (currentHour >= 18 && currentHour < 22){
-    message = "Rango da noite, vamos jantar :D";
-}
-else if (currentHour >= 14 && currentHour < 18){
-    message = "Vamos fazer um bolo pro café da tarde?";
-}
-else if(currentHour >= 11 && currentHour < 14){
-    message = "Hora do almoço!!!";
-}
-else if (currentHour >= 4 && currentHour < 11){
-    message = "Hmmm, cheiro de café recém passado";
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    message = "Oba, mais um dia de aprendizado na Trybe >:D";
 }
 
+else {
+    message = "FINALMENTE, descanso merecido UwU"; // GZUS, UwU?????
+}
 console.log(message);
