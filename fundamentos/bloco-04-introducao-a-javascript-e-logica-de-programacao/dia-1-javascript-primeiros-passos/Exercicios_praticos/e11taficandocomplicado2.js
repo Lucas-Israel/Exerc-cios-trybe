9ea831606c9d68dc8a59ed198d6b33e78229f927
Lucas Israel <lisraelalves92@gmail.com>
2022-05-13 09:27:13 -0300

    arrumando indent

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicios_praticos/e11taficandocomplicado2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicios_praticos/e11taficandocomplicado2.js
@@ -6,48 +6,50 @@ let aliquotaIR = "";
 
 
 if (salario < INSS1[0]){
-    aliquotaINSS = 1.08;
+  aliquotaINSS = 1.08;
 }
 else if (salario >= INSS1[0] && salario < INSS1[1]){
-    aliquotaINSS = 1.09;
+  aliquotaINSS = 1.09;
 }
 else if (salario >= INSS1[1] && salario < INSS1[2]){
-    aliquotaINSS = 1.11;
+  aliquotaINSS = 1.11;
 }
 else {
-    aliquotaINSSMAX = 570.88;
+  aliquotaINSSMAX = 570.88;
 }
+
 
 let reducaoINSS = salario * aliquotaINSS - salario;
 let salarioBase = salario - reducaoINSS
 
+
 if (salario > INSS1[2]) {
-    salarioBase = salario - aliquotaINSSMAX;
+  salarioBase = salario - aliquotaINSSMAX;
 }
 else {
-    aliquotaINSSMAX = 570.88
+  aliquotaINSSMAX = 570.88
 }
 
 
 if (salarioBase < IR1[0]){
-    aliquotaIR = 1;
-    parcelaIR = 0;
+  aliquotaIR = 1;
+  parcelaIR = 0;
 }
 else if (salarioBase >= IR1[0] && salarioBase < IR1[1]){
-    aliquotaIR = 1.075; 
-    parcelaIR = 142.80;
+  aliquotaIR = 1.075; 
+  parcelaIR = 142.80;
 }
 else if (salarioBase >= IR1[1] && salarioBase < IR1[2]){
-    aliquotaIR = 1.15;
-    parcelaIR = 354.80;
+  aliquotaIR = 1.15;
+  parcelaIR = 354.80;
 }
 else if (salarioBase >= IR1[2] && salarioBase <= IR1[3]){
-    aliquotaIR = 1.225; 
-    parcelaIR = 636.13;
+  aliquotaIR = 1.225; 
+  parcelaIR = 636.13;
 }
 else {
-    aliquotaIR = 1.275;
-    parcelaIR = 869.36;
+  aliquotaIR = 1.275;
+  parcelaIR = 869.36;
 }
 
 
@@ -59,5 +61,5 @@ const salarioLiq = (Math.round((salarioBase - salarioParc)*100)/100);
 console.log("Salario Bruto R$:"+salario);
 console.log("Aliquota INSS:*"+aliquotaINSS);
 console.log("Aliquota INSSMAX R$:"+aliquotaINSSMAX)
-console.log("Aliquota IR:*"+aliquotaIR,"   Parcela de IR R$:" +parcelaIR);
+console.log("Aliquota IR:*"+aliquotaIR,"  Parcela de IR R$:" +parcelaIR);
 console.log("Salario Liquido R$:"+salarioLiq);
