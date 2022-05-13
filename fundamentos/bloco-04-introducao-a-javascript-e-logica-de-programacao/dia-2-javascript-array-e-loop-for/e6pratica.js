438624da0ba22c87a0968839e2f02454bd2007d8
Lucas Israel <lisraelalves92@gmail.com>
2022-05-13 19:22:40 -0300

    exercicio 6

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/e6pratica.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/e6pratica.js
@@ -1,11 +1,19 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let a = 0;
+let qntImpar = 0;
 
 for (let i = 0; i < numbers.length; i+=1) {
-    if ( numbers[i]%2 == 0 ){
-        console.log("Par")
+    if ( numbers[i]%2 == 1 ){
+        qntImpar += 1;
     }
     else {
-        console.log("Impar")
+        
     }
+}
+
+if(qntImpar>0){
+    console.log("Existem "+qntImpar+" impares")
+}
+else {
+    console.log("Não existe impar")
 }
