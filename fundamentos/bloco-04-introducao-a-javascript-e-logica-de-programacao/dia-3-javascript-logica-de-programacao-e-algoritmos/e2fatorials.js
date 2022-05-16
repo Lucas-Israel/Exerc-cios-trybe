2e5f2f8fc08af8de78f3cb0822403f2bd9c38e85
Lucas Israel <lisraelalves92@gmail.com>
2022-05-16 16:22:50 -0300

    e2 feito

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/e2fatorials.js
+++
@@ -1,5 +0,0 @@
-let word = ['t','r','y','b','e','r'];
-
-word.reverse();
-
-console.log(word)
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/e2invertendo.js
@@ -0,0 +1,8 @@
+let word = 'tryber';
+let a = Array.from(word);
+
+a.reverse();
+
+let b = a.join('');
+
+console.log(b);
