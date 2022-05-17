a773f49884c8ff204d706da2a881f63965b9c2ac
Lucas Israel <lisraelalves92@gmail.com>
2022-05-17 15:27:09 -0300

    fixacao dos objetos

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/c1objetosexemplos.js
+++
@@ -1,9 +0,0 @@
-let car = {
-    type: 'Fiat',
-    model: '500',
-    color: 'white',
-  };
-  console.log(car.type)
-  console.log(car.model)
-  console.log(car.color)
-  console.log(car)
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/c2objetos.js
@@ -0,0 +1,15 @@
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { 
+        golden: 2, 
+        silver: 3 
+        },
+}
+
+let fullName = player.name +" "+player.lastName
+console.log(player.name)
+console.log(player.lastName)
+console.log(player.age)
+console.log("A jogadora "+fullName+ " tem "+player.age+ " anos de idade")
