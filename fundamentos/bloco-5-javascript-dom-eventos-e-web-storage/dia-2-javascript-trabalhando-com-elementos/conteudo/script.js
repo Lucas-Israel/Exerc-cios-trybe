172df0002fc5c24fc14bd10238477a654834ff48
Lucas Israel <lisraelalves92@gmail.com>
2022-05-20 18:09:25 -0300

    feito conteudo

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/conteudo/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/conteudo/script.js
@@ -1,16 +1,30 @@
 let b1 = document.getElementById('elementoOndeVoceEsta');
-console.log(b1);
+// console.log(b1);
 let parentb1 = b1.parentElement;
 parentb1.style.color = 'red';
-let pneto = document.getElementById('primeiroFilhoDoFilho');
-pneto.innerText = 'adicionando texto';
+let neto = document.getElementById('primeiroFilhoDoFilho');
+neto.innerText = 'adicionando texto';
 let pai = document.getElementById('pai');
 let pFiPai = pai.firstElementChild;
-console.log(pFiPai);
-console.log(b1.previousElementSibling);
+// console.log(pFiPai);
+// console.log(b1.previousElementSibling);
 let atenOnde = b1.nextSibling;
-console.log(atenOnde);
+// console.log(atenOnde);
 let terFiOnde = b1.nextElementSibling;
-console.log(terFiOnde);
+// console.log(terFiOnde);
 let terFiPai = pai.lastElementChild.previousElementSibling;
-console.log(terFiPai);
+// console.log(terFiPai);
+
+const irmEleOnVcEs = document.createElement('section');
+irmEleOnVcEs.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmEleOnVcEs);
+
+const eleChild = document.createElement('section');
+eleChild.id = 'childElementoOndeVoceEsta';
+b1.appendChild(eleChild);
+
+const bisneto = document.createElement('section');
+bisneto.id = 'neto';
+neto.appendChild(bisneto);
+
+console.log(bisneto.parentElement.parentElement.nextElementSibling)
