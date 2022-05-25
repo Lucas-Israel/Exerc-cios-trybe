fbbe070e01e381a7863d9133a68809058eab0d08
Lucas Israel <lisraelalves92@gmail.com>
2022-05-24 22:17:27 -0300

    fazendo as coisas deixou mais baguncado

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/script.js
@@ -5,7 +5,7 @@ window.onload = function(){
         document.body.style.backgroundColor = selected.value;
 
         localStorage.setItem('5.4-background-color', selected.value);
-    })
+    });
 
     let inputFontSize = document.querySelector('input[name="font-size"]');
     inputFontSize.addEventListener('change', function(){
@@ -13,7 +13,7 @@ window.onload = function(){
         p.style.fontSize = `${inputFontSize.value}px`;
 
         localStorage.setItem('5.4-fontsize', `${inputFontSize.value}px`)
-    })
+    });
 
     let textColor = document.querySelector('#text-color');
     textColor.addEventListener('change', function(){
@@ -22,16 +22,44 @@ window.onload = function(){
         paragrafos.style.color = txtc.value;
 
         localStorage.setItem('5.4-font-color', txtc.value);
-    })
+    });
+
+    let fonteB1 = document.querySelector('#b1');
+    let fonteB2 = document.querySelector('#b2');
+    fonteB1.addEventListener('click', function(){
+        let paragrafos = document.getElementById('conteudo');
+        paragrafos.style.fontFamily = fonteB1.value;
+
+        localStorage.setItem('5.4-font-family', fonteB1.value);
+    });
+    fonteB2.addEventListener('click', function(){
+        let paragrafos = document.getElementById('conteudo');
+        paragrafos.style.fontFamily = fonteB2.value;
+
+        localStorage.setItem('5.4-font-family', fonteB2.value);
+    });
+
+    let espaLin = document.querySelector('input[name="esp-lin"]');
+    espaLin.addEventListener('change', function(){
+        let p = document.querySelector('p');
+        p.style.lineHeight = `${espaLin.value}px`;
+
+        localStorage.setItem('5.4-espaLin', `${espaLin.value}px`)
+    });
 
 
 
+    
     let savedBackGround = localStorage.getItem('5.4-background-color');
     document.body.style.backgroundColor = savedBackGround;
 
     let savedFontSize = localStorage.getItem('5.4-fontsize');
-    document.body.style.fontSize = savedFontSize;
+    let fontSave = document.querySelector('#conteudo');
+    fontSave.style.fontSize = savedFontSize;
 
     let savedTxt = localStorage.getItem('5.4-font-color');
     document.body.style.color = savedTxt;
+
+    let fonteF = localStorage.getItem('5.4-font-family');
+    document.body.p.style.fontFamily = fonteF;
 }
