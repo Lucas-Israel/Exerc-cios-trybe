fe2f22e41f32c63c40347e5336b4611c930c4e57
Lucas Israel <lisraelalves92@gmail.com>
2022-06-09 19:04:09 -0300

    terminado exe 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -1,6 +1,8 @@
 const check = (value1, value2) => {
   if (value1 === ''|| value2 === '') {
     throw new Error ('Não deixar nenhuma caixa vazia.')
+  } else if (isNaN(value1) || isNaN(value2)) {
+    throw new Error ('Usar apenas números')
   }
 };
 
@@ -11,10 +13,11 @@ function sum() {
   try {
     check(value1, value2);
     document.getElementById('result').innerText = `Resultado: ${result}`;
-    document.getElementById('value1').value = '';
-    document.getElementById('value2').value = '';
   } catch (error) {
     return alert(error.message);
+  } finally {
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
   }
 }
 
