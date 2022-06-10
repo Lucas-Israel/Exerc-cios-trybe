508fd94ac95ea6af47e218bc1f3ca3d6174ab6bb
Lucas Israel <lisraelalves92@gmail.com>
2022-06-10 14:38:43 -0300

    bloco 6

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/exercicios/script.js
@@ -0,0 +1,27 @@
+import JustValidate from 'just-validate';
+
+const validate = new JustValidate('#form');
+
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ]);
