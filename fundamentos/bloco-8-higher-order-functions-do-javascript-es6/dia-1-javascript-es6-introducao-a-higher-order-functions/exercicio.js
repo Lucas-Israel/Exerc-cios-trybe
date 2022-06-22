8014905b3c6a03476445f5b72d556ef7d100d1da
Lucas Israel <lisraelalves92@gmail.com>
2022-06-22 16:23:40 -0300

    e3 feito

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio.js
@@ -13,18 +13,35 @@
 
 // e1 /\
 
-const apo = (a) => a;
+// const apo = (a) => a;
 
-const check = (a, b) => {
-  if (a === b) return 'Parabéns você ganhou';
-  return 'Tente novamente';
-}
+// const check = (a, b) => {
+//   if (a === b) return 'Parabéns você ganhou';
+//   return 'Tente novamente';
+// }
 
-const tesouro = (a) => {
-  let b = Math.floor(Math.random() * 6);
-  return check(a, b);
-}
+// const tesouro = (a) => {
+//   let b = Math.floor(Math.random() * 5);
+//   return check(a, b);
+// }
 
-console.log(tesouro(apo(5)));
+// console.log(tesouro(apo(5)));
 
 // e2 /\
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const che = (a, b) => {
+  let cont = 0;
+  for (let index = 0; index < a.length; index += 1) {
+    if (b[index] === 'N.A') cont += 0.5;
+    if (a[index] === b[index]) cont += 1;
+    if (a[index] !== b[index]) cont -= 0.5;
+  }
+  return cont;
+}
+
+const abc = (a, b, c) => console.log(a(b, c));
+
+abc(che, RIGHT_ANSWERS, STUDENT_ANSWERS);
