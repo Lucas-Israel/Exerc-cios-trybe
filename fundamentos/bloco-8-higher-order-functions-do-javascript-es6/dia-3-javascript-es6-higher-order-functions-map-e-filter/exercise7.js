176965f435a32a6b5c65083ecec73f6028329122
Lucas Israel <lisraelalves92@gmail.com>
2022-06-25 14:02:12 -0300

    FEITO COMO EU QUERIA DESDE O INICIO

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
@@ -3,13 +3,8 @@ const books = require ('./books.js');
 // 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
 // Dica: cada inicial termina com um ponto.
 
-const abc = (a) => a.find(a => a.author.name.split(' ').filter(a => a.endsWith('.')).length === 3).name
+const bcd = /.\. .\. .\. \w*/g;
+
+const abc = (a) => a.filter(a => a.author.name.match(bcd))[0].author.name;
 
 console.log(abc(books));
-
-
-// perguntar se esta tudo bem eu ter usado .mjs para exportar e importar no exercicio que pedia para colocar os exercicios em arquivos diferentes .js
-
-// perguntar sobre o funcionamento do exercicio 7 a partir do split para ver se eu entendi corretamente.
-
-// split da push em um array com o parametro passado, entao toda palavra que tiver o parametro no final é uma entrada nova no array e endsWith verifica se o final de cada string dentro da array termina com o parametro passado.
