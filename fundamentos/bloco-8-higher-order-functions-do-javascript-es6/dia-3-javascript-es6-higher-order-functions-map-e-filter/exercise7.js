852242c066f439a8cda83878dc02899a4dd70586
Lucas-Israel <104790267+Lucas-Israel@users.noreply.github.com>
2022-06-26 12:14:01 -0300

    Update exercise7.js

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise7.js
@@ -3,7 +3,7 @@ const books = require ('./books.js');
 // 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
 // Dica: cada inicial termina com um ponto.
 
-const bcd = /.\. .\. .\. \w*/g;
+const bcd = /(.\. ){3}/g;
 
 const abc = (a) => a.filter(a => a.author.name.match(bcd)).map(a => a.author.name)
 
