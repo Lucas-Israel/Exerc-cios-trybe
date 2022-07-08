b46d799a983eb32e904098fa58968d7a7b5450a5
Lucas Israel <lisraelalves92@gmail.com>
2022-07-08 11:38:20 -0300

    exercicios revisados e bonus feito

---
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicios/apiScript.js
@@ -0,0 +1,17 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const container = document.getElementById('jokeContainer');
+  const myObject = {
+      method: 'GET',
+      headers: { 'Accept': 'application/json' }
+    };
+  
+    fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then((data) => {
+      container.innerText = data.joke
+    });
+};
+
+window.onload = () => fetchJoke();
