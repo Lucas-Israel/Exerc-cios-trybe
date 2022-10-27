d04913a0dc4d814cb83bc4cc976581cb2a621078
Lucas Israel <lisraelalves92@gmail.com>
2022-10-27 20:28:48 -0300

    feito

--- back-end/secao-desenvolvimento-web-com-nodeJs/dia-01-nodejs/index.js
+++ back-end/secao-desenvolvimento-web-com-nodeJs/dia-01-nodejs/index.js
@@ -3,6 +3,11 @@ const s = require('./simpsons');
 
 // console.log(IMC(100,200));
 
-s.simPersonagem();
-s.search(2);
-s.search(20);
+// s.simPersonagem();
+// s.search(2);
+// s.search(20);
+// s.remove(10);
+// s.search(10);
+// s.family();
+// s.intruso();
+s.corrige();
--- back-end/secao-desenvolvimento-web-com-nodeJs/dia-01-nodejs/simpsons.js
+++ back-end/secao-desenvolvimento-web-com-nodeJs/dia-01-nodejs/simpsons.js
@@ -14,7 +14,7 @@ const simPersonagem = async () => {
 const search = async (id) => {
   const data = await fs.readFile('./simpsonsMock.json', 'utf-8');
   const json = JSON.parse(data);
-  const map = json.find((p) => p.id == id)
+  const map = json.find((p) => +p.id === id)
   if (!map) return console.log('id não encontrado');
   console.log(map);
 }
@@ -22,11 +22,53 @@ const search = async (id) => {
 const remove = async (id) => {
   const data = await fs.readFile('./simpsonsMock.json', 'utf-8');
   const json = JSON.parse(data);
-  const map = json.filter((p) => p.id == id)
+  const map = json.filter((p) => +p.id !== id)
+  const string = JSON.stringify(map); 
+  fs.writeFile('./simpsonsMock.json', string);
+}
+
+const family = async () => {
+  const data = await fs.readFile('./simpsonsMock.json', 'utf-8');
+  const json = JSON.parse(data);
+  const map = json.filter(({id}) => {
+      if (+id === 1) return true;
+      if (+id === 2) return true;
+      if (+id === 3) return true;
+      if (+id === 4) return true;
+      return false;
+  })
+  const string = JSON.stringify(map); 
+  fs.writeFile('./simpsonFamily.json', string);
+}
+
+const intruso = async () => {
+  const data = await fs.readFile('./simpsonsMock.json', 'utf-8');
+  const familydata = await fs.readFile('./simpsonFamily.json', 'utf-8');
+  const json = JSON.parse(data);
+  const jsonFam = JSON.parse(familydata);
+  const map = json.find(({name}) => name === 'Nelson Muntz');
+  const mappingFam = [...jsonFam, map];
+  const string = JSON.stringify(mappingFam); 
+  fs.writeFile('./simpsonFamily.json', string);
+}
+
+const corrige = async () => {
+  const data = await fs.readFile('./simpsonsMock.json', 'utf-8');
+  const familydata = await fs.readFile('./simpsonFamily.json', 'utf-8');
+  const json = JSON.parse(data);
+  const jsonFam = JSON.parse(familydata);
+  const map = json.find(({name}) => name === 'Maggie Simpson');
+  const filtering = jsonFam.filter(({name}) => name !== 'Nelson Muntz');
+  const mappingFam = [...filtering, map];
+  const string = JSON.stringify(mappingFam); 
+  fs.writeFile('./simpsonFamily.json', string);
 }
 
 module.exports = {
   simPersonagem,
   search,
   remove,
+  family,
+  intruso,
+  corrige,
 };
