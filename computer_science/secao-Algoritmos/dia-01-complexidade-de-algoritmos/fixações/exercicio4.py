d9479bed9774319cf7a395102a590a6ae2e83927
Lucas Israel <lisraelalves92@gmail.com>
2023-04-19 18:33:51 -0300

    feat: :sparkles: conteúdo e exercícios do dia feitos

---
+++ computer_science/secao-Algoritmos/dia-01-complexidade-de-algoritmos/fixações/exercicio4.py
@@ -0,0 +1,35 @@
+def binary_search(numbers, target):
+    # definir os índices
+    start = 0
+    end = len(numbers) - 1
+
+    while start <= end:  # os índices podem ser no máximo iguais, o início não
+        # pode ultrapassar o fim
+        mid = (start + end) // 2  # encontro o meio
+
+        if numbers[mid] == target:  # se o elemento do meio for o alvo,
+            # devolve a posição do meio
+            return mid
+
+        if target < numbers[mid]:  # se o elemento for menor, atualiza o
+            # índice do fim
+            end = mid - 1
+        else:  # caso contrário, atualiza o índice do inicio
+            start = mid + 1
+
+    return -1  # Não encontrou? Retorna -1
+
+
+def do_something(array1, array2):
+    a = 0
+    for number in array1:  # O (n)
+        a = binary_search(array2, number)  # O (log n)
+    return a
+
+
+if __name__ == '__main__':
+    n = 51
+    meu_array = list(range(1, n))
+
+    a = do_something(meu_array, meu_array)
+    print(a)
