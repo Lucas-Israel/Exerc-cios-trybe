de95f0ae6a98af4894cb94984fdd8f772efd7aa6
Lucas Israel <lisraelalves92@gmail.com>
2023-05-15 15:26:35 -0300

    feat: :sparkles: conteudo

---
+++ computer_science/secao-estrutura-de-dados-II/dia-03-pandas/fixação/exercicio_2.py
@@ -0,0 +1,14 @@
+# A partir do dicionário criado no exercício 1, armazene os DataSeries
+# referentes a cada uma das colunas em variáveis e imprima a saída.
+
+from exercicio_1 import d_frame
+
+serie_1 = d_frame["amigos"]
+serie_2 = d_frame["prato"]
+serie_3 = d_frame["sobremesa"]
+
+print(serie_1)
+print("----------------")
+print(serie_2)
+print("----------------")
+print(serie_3)
